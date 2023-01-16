516fc171c1edb1bdb50f162cf9ca5945d31949f1
Marinus <marinus.fehling@web.de>
2023-01-16 11:33:37 +0100

    challenge gelöst

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,10 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+let zahl1, zahl2, summe; // Deklaration
+zahl1 = prompt("Bitte eine Zahl eingeben:");  // Wertzuweisung
+zahl2 = prompt("Bitte eine weitere Zahl eingeben:");  // Wertzuweisung
+
+summe = parseInt(zahl1) + parseInt(zahl2);  // Umwandlung String -> Number; Summe berechnen
+console.log("Die Summe aus beiden Zahlen ist " + summe); // Ausgabe
